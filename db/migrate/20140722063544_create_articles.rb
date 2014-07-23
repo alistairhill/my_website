399efaab05b3b8c431e130f78cf77c6e07709dc8
Alistair <plantonly@gmail.com>
2014-07-22 19:31:43 -0700

    added content to migration files

--- db/migrate/20140722063544_create_articles.rb
+++ db/migrate/20140722063544_create_articles.rb
@@ -1,6 +1,10 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
+      t.string :title
+      t.text :body
+      t.references :user
+      t.timestamps
     end
   end
 end
--- db/migrate/20140722063600_create_comments.rb
+++ db/migrate/20140722063600_create_comments.rb
@@ -1,6 +1,10 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.string :body
+      t.references :commentable, polymorphic: true
+      t.references :user
+      t.timestamps
     end
   end
 end
