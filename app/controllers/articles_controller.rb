5fbffaa87ef6e26ccd3e765d834f4fc193137f90
Alistair <plantonly@gmail.com>
2014-07-22 23:07:11 -0700

    added article, comments, user and session controllers

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,35 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+    if params[:sort_type] == "recent"
+      @articles = Article.all.order(created_at: :desc)
+    end
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article Article.new(article_params)
+    @article.user_id = logged_in
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end
