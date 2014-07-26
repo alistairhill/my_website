852ef5fcb17a40f4a2428de2b2e2c273b7fd3381
Alistair <plantonly@gmail.com>
2014-07-25 23:58:02 -0700

    fine tuning blog page post data

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @username = @article.user.username
+    # @username = @article.user.username
     @article.user_id = logged_in
     if @article.save
       redirect_to articles_path
@@ -27,6 +27,12 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    redirect_to articles_path
   end
 
   private
